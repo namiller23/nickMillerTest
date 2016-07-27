15e405c29c7545143785c9620ff85c3695ba0474
namiller23 <namiller23@gmail.com>
2016-07-27 18:05:37 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function callFourSquare() {
 
+  var exploreURL="https://api.foursquare.com/v2/venues/explore?";
+  var clientId="RGIHNQWTCTBBQWAZEUZZKILDWF0EAQERVTXJUI23QU40ZZYJ";
+  var clientSecret="ULCGZV4SQQNFZ21VPGK455SYCS4QSNHP1INNF1CZ21LPBF0E";
+  var near=$("#theInput").val();
+
+  $.getJSON(exploreURL+"client_id="+clientId+"&client_secret="+clientSecret+"&v=20160601&near="+near,
+    function(data) {
+        $.each(data.response.groups[0].items, function(i,venues){
+            content = '<p>' + venues.venue.name + '</p>';
+            $(content).appendTo("#names");
+       });
+});
+}
