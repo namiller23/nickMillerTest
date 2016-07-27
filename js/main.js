0f5c03823dd4ab591ed36c791af8a5f402445cee
namiller23 <namiller23@gmail.com>
2016-07-27 19:44:06 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 function callFourSquare() {
 
-
-
   var exploreURL="https://api.foursquare.com/v2/venues/explore?";
   var clientId="RGIHNQWTCTBBQWAZEUZZKILDWF0EAQERVTXJUI23QU40ZZYJ";
   var clientSecret="ULCGZV4SQQNFZ21VPGK455SYCS4QSNHP1INNF1CZ21LPBF0E";
@@ -9,11 +7,11 @@ function callFourSquare() {
 
   if((exploreURL||clientId||clientSecret)==null)
       {
-        alert("an error has occurred");
+        alert("An error has occurred, please try again later");
 
       }else if(!near || near.length==0)
       {
-        alert("please enter a value");
+        alert("Please enter a search value");
         
       }else
       {
@@ -27,19 +25,31 @@ function getResponse(exploreURL,clientId,clientSecret,near)
   $.getJSON(exploreURL+"client_id="+clientId+"&client_secret="+clientSecret+"&v=20160601&near="+near,
     function(data) {
         $.each(data.response.groups, function(i, groups){
-            content = '<h3>' + groups.type+ '</h3>';
+            content = '<div class="group" id="group'+i+'">' + groups.type+ '</div>';
             $(content).appendTo("#results");
-            $.each(groups.items, function(i, item){
-              createItem(item);
+            $.each(groups.items, function(j, item){
+              createItem(item, i, j);
             });
        });
   });
 
 }
 
-function createItem(item)
+function createItem(item, i, j)
+
 {
-  
+    var name = item.venue.name;
+    var category = item.venue.categories[0].name;
+    var rating = item.venue.rating;
+    var address = item.venue.location.formattedAddress;
+
+    itemContent ='<div class ="item" id="item'+j+'">' + 
+    '<p>Name: '+name+'</p>'+ 
+    '<p>Category: '+category+'</p>'+ 
+    '<p>Rating: '+rating+'</p>'+ 
+    '<p>Address: '+address+'</p>'
+    +'</div>';
+    $(itemContent).appendTo("#group"+i);
 }
 
 
